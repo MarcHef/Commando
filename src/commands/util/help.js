9e8fd2890c869962d35e68503b23e5298fc4be36
Marc Heffels <mail@marc-heffels.de>
2020-04-26 00:23:50 +0200

    Add i18n to help.js

--- src/commands/util/help.js
+++ src/commands/util/help.js
@@ -36,68 +36,59 @@ module.exports = class HelpCommand extends Command {
 			if(commands.length === 1) {
 				let help = stripIndents`
 					${oneLine`
-						__Command **${commands[0].name}**:__ ${commands[0].description}
-						${commands[0].guildOnly ? ' (Usable only in servers)' : ''}
-						${commands[0].nsfw ? ' (NSFW)' : ''}
+						__${this.client.i18n.__('Command')} **${commands[0].name}**:__ ${this.client.i18n.__(commands[0].description)}
+						${commands[0].guildOnly ? ' ('+this.client.i18n.__('Usable only in servers')+')' : ''}
+						${commands[0].nsfw ? ' ('+this.client.i18n.__('NSFW')+')' : ''}
 					`}
 
-					**Format:** ${msg.anyUsage(`${commands[0].name}${commands[0].format ? ` ${commands[0].format}` : ''}`)}
+					**${this.client.i18n.__('Format')}:** ${msg.anyUsage(`${commands[0].name}${commands[0].format ? ` ${commands[0].format}` : ''}`)}
 				`;
-				if(commands[0].aliases.length > 0) help += `\n**Aliases:** ${commands[0].aliases.join(', ')}`;
+				if(commands[0].aliases.length > 0) help += `\n**${this.client.i18n.__('Aliases')}:** ${commands[0].aliases.join(', ')}`;
 				help += `\n${oneLine`
-					**Group:** ${commands[0].group.name}
+					**${this.client.i18n.__('Group')}:** ${commands[0].group.name}
 					(\`${commands[0].groupID}:${commands[0].memberName}\`)
 				`}`;
-				if(commands[0].details) help += `\n**Details:** ${commands[0].details}`;
-				if(commands[0].examples) help += `\n**Examples:**\n${commands[0].examples.join('\n')}`;
+				if(commands[0].details) help += `\n**${this.client.i18n.__('Details')}:** ${this.client.i18n.__(commands[0].details)}`;
+				if(commands[0].examples) help += `\n**${this.client.i18n.__('Examples')}:**\n${commands[0].examples.join('\n')}`;
 
 				const messages = [];
 				try {
 					messages.push(await msg.direct(help));
-					if(msg.channel.type !== 'dm') messages.push(await msg.reply('Sent you a DM with information.'));
+					if(msg.channel.type !== 'dm') messages.push(await msg.reply(msg.client.i18n.__('Sent you a DM with information.')));
 				} catch(err) {
-					messages.push(await msg.reply('Unable to send you the help DM. You probably have DMs disabled.'));
+					messages.push(await msg.reply(msg.client.i18n.__('Unable to send you the help DM. You probably have DMs disabled.')));
 				}
 				return messages;
-			} else if(commands.length > 15) {
-				return msg.reply('Multiple commands found. Please be more specific.');
 			} else if(commands.length > 1) {
-				return msg.reply(disambiguation(commands, 'commands'));
+				return msg.reply(disambiguation(msg.client, commands, 'commands'));
 			} else {
-				return msg.reply(
-					`Unable to identify command. Use ${msg.usage(
-						null, msg.channel.type === 'dm' ? null : undefined, msg.channel.type === 'dm' ? null : undefined
-					)} to view the list of all commands.`
-				);
+				return msg.reply(msg.client.i18n.__('Unknown command. Use %s to view the command list.', msg.usage(null, msg.channel.type === 'dm' ? null : undefined, msg.channel.type === 'dm' ? null : undefined )));
 			}
 		} else {
 			const messages = [];
 			try {
 				messages.push(await msg.direct(stripIndents`
-					${oneLine`
-						To run a command in ${msg.guild ? msg.guild.name : 'any server'},
-						use ${Command.usage('command', msg.guild ? msg.guild.commandPrefix : null, this.client.user)}.
-						For example, ${Command.usage('prefix', msg.guild ? msg.guild.commandPrefix : null, this.client.user)}.
-					`}
-					To run a command in this DM, simply use ${Command.usage('command', null, null)} with no prefix.
+					${msg.client.i18n.__('To run a command in %s, use %s. For example, %s.', msg.guild ? msg.guild.name : msg.client.i18n.__('any server'), Command.usage('command', msg.guild ? msg.guild.commandPrefix : null, this.client.user), Command.usage('prefix', msg.guild ? msg.guild.commandPrefix : null, this.client.user))}
+					
+					${msg.client.i18n.__('To run a command in this DM, simply use %s with no prefix.', Command.usage('command', null, null))}
 
-					Use ${this.usage('<command>', null, null)} to view detailed information about a specific command.
-					Use ${this.usage('all', null, null)} to view a list of *all* commands, not just available ones.
+					${msg.client.i18n.__('Use %s to view detailed information about a specific command.', this.usage('<command>', null, null))}
+					${msg.client.i18n.__('Use %s to view a list of *all* commands, not just available ones.', this.usage('all', null, null))}
 
-					__**${showAll ? 'All commands' : `Available commands in ${msg.guild || 'this DM'}`}**__
+					__**${showAll ? msg.client.i18n.__('All commands') : `${msg.client.i18n.__('Available commands in %s', msg.guild || 'DM')}`}**__
 
 					${groups.filter(grp => grp.commands.some(cmd => !cmd.hidden && (showAll || cmd.isUsable(msg))))
 						.map(grp => stripIndents`
 							__${grp.name}__
 							${grp.commands.filter(cmd => !cmd.hidden && (showAll || cmd.isUsable(msg)))
-								.map(cmd => `**${cmd.name}:** ${cmd.description}${cmd.nsfw ? ' (NSFW)' : ''}`).join('\n')
+								.map(cmd => `**${cmd.name}:** ${msg.client.i18n.__(cmd.description)}${cmd.nsfw ? ' (NSFW)' : ''}`).join('\n')
 							}
 						`).join('\n\n')
 					}
 				`, { split: true }));
-				if(msg.channel.type !== 'dm') messages.push(await msg.reply('Sent you a DM with information.'));
+				if(msg.channel.type !== 'dm') messages.push(await msg.reply(msg.client.i18n.__('Sent you a DM with information.')));
 			} catch(err) {
-				messages.push(await msg.reply('Unable to send you the help DM. You probably have DMs disabled.'));
+				messages.push(await msg.reply(msg.client.i18n.__('Unable to send you the help DM. You probably have DMs disabled.')));
 			}
 			return messages;
 		}
