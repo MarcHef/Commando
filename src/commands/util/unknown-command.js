8b30ced2a6f3d6954e15f07408b02d49ddf5c945
Marc Heffels <mail@marc-heffels.de>
2020-04-26 00:23:12 +0200

    Add i18n to unknown-command.js

--- src/commands/util/unknown-command.js
+++ src/commands/util/unknown-command.js
@@ -14,12 +14,6 @@ module.exports = class UnknownCommandCommand extends Command {
 	}
 
 	run(msg) {
-		return msg.reply(
-			`Unknown command. Use ${msg.anyUsage(
-				'help',
-				msg.guild ? undefined : null,
-				msg.guild ? undefined : null
-			)} to view the command list.`
-		);
+		return msg.reply(msg.client.i18n.__('Unknown command. Use %s to view the command list.', msg.anyUsage('help', msg.guild ? undefined : null, msg.guild ? undefined : null )));
 	}
 };
